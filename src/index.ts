d1f597b9387591ec7bf1bbd537193a427eb05612
Piyushkumar-20 <pk6686742@gmail.com>
2026-06-06 19:51:41 +0530

    chore: Update TypeScript configuration and package settings
    - Changed module and moduleResolution in tsconfig.json to CommonJS and node for better compatibility.
    - Removed type: module from package.json to align with the updated TypeScript settings.
    - Cleaned up import statements in src/index.ts by removing .js extensions for consistency.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import express from "express";
 import path from "node:path";
 import jose from "node-jose";
 import crypto from "node:crypto";
-import { PUBLIC_KEY, PRIVATE_KEY } from "./utils/cert.js";
-import { db } from "./db/index.js";
+import { PUBLIC_KEY, PRIVATE_KEY } from "./utils/cert";
+import { db } from "./db/index";
 import { and, eq } from "drizzle-orm";
 import {
   userTable,
   applicationTable,
   authorizationCodeTable,
   refreshTokenTable,
-} from "./db/schema.js";
-import type { JWTClaims } from "./utils/user-token.js";
+} from "./db/schema";
+import type { JWTClaims } from "./utils/user-token";
 import JWT from "jsonwebtoken";
 
 const app = express();
